79aed8ee1325fcf8883f22c62dc05dc2e13338b6
de-scientist <gitaumark502@gmail.com>
2025-11-07 11:36:06 +0300

    components are reusable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // learn that a component can only return one Html element
 //learn that to have more then one html element we should nest inside containers such as divs
 //we use pascal case convention when creating components(first character is uppercase)
-
+//components are reusable
 
 
 function App () {
